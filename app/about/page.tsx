9e04d0fe4bfb3e4d8b99979778aa2e9b2e3d55ed
Eric Reeves <leagues58@gmail.com>
2025-03-07 14:17:43 -0500

    Added an about page

---
+++ app/about/page.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        About
+      </h1>
+      <div className="my-8 flex flex-row">
+        <div className={"px-10"}>
+            This is where stuff about me will go!
+            I'm difficult to get a hold of, but please try my pidgeon service if email doesn't work.
+        </div>
+        <Image className={"px-10"} src={'/img/me.jpg'} alt="a picture of me" width={400} height={400}/>
+      </div>
+    </section>
+  )
+}
