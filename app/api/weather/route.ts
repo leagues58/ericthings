39fc5198902ec7ebb70cb3b70760980741753eaa
Eric Reeves <leagues58@gmail.com>
2025-03-22 09:01:40 -0400

    Added ambientweather api endpoint

---
+++ app/api/weather/route.ts
@@ -0,0 +1,25 @@
+export async function GET(request: Request) {
+  let apiKey = process.env.AMBIENT_WEATHER_API_KEY;
+  let applicationKey = process.env.AMBIENT_WEATHER_APPLICATION_KEY;
+  let URL = 'https://api.ambientweather.net/v1/devices/'
+  let weatherData;
+  let weatherResponse = {
+    error: true,
+    message: '',
+    data: {}
+  }
+
+  if (!apiKey || !applicationKey) {
+    return Response.json({...weatherResponse, message: 'API keys not configured.'});
+  }
+
+  let response = await fetch(`${URL}?apiKey=${apiKey}&applicationKey=${applicationKey}`, { cache: 'no-store' });
+  
+  if (response.ok) {
+    weatherData = await response.json();
+
+    return Response.json({...weatherResponse, error: false, data: weatherData[0]});
+  }
+
+  return Response.json({...weatherResponse, message: 'No response from AmbientWeather.'});
+}
